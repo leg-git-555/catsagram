ce003991c762e67e7bee4fb647197e75d2fc68f3
Nicholas Leger <nick.leger2314@gmail.com>
2023-11-30 18:50:23 -0500

    phase2 complete

--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
 // Your code here 
-//import { makeDivWText } from "./helpers"
 
 let randomCatImg = async () => {
     let res = await fetch("https://api.thecatapi.com/v1/images/search")
+        //console.log(res)
     let parsedRes = await res.json()
+        //console.log(parsedRes)
     let url = parsedRes[0].url
 
     let img = document.createElement("img")
@@ -12,20 +13,76 @@ let randomCatImg = async () => {
      return img
 }
 
-// let append = (parent, child) => {
-//     return parent.append(child)
-// }
 
 
 const initializePage = async () => {
 
 
     let catImg = await randomCatImg()
-    //console.log(catImg)
+
     let imgBox = document.getElementById("img-box")
-    console.log(imgBox)
+        imgBox.append(catImg)
+
+    //spawn new cat and reset popularity and comments
+    let newCatButton = document.querySelector("#spawn-cat")
     
-    imgBox.append(catImg)
+        newCatButton.addEventListener("click", async (event) => {
+            let newCat = await randomCatImg()
+            catImg.src = newCat.src
+
+                count = 0
+                catScore.innerText = count
+                
+                //let lisQ = document.querySelectorAll("li")
+                //console.log('lis with query:', lisQ)
+
+                let lisG = document.getElementsByTagName("li")
+                let lisGcopy = [...lisG]
+                console.log('lisGcopy:',lisGcopy)
+
+               if (lisG) {
+                lisGcopy.forEach(li => li.remove())
+               }
+
+        })
+        
+
+    var count = 0
+    var catScore = document.getElementById("score")
+
+
+    let upVote = document.querySelector("#upvote")
+
+        upVote.addEventListener("click", (event) => {
+            count++
+            catScore.innerText = count
+
+        })
+
+    let downVote = document.querySelector("#downvote")
+
+        downVote.addEventListener("click", e => {
+            count--
+            catScore.innerText = count
+        })
+
+    //adding comments to ul
+    let commentButton = document.querySelector("#cat-comment-button")
+    let inputArea = document.getElementById("cat-comment")
+    var ul = document.getElementsByTagName("ul")[0]
+    //var lis = document.querySelectorAll("li")
+
+        commentButton.addEventListener("click", (e) => {
+            let comment = inputArea.value
+            let li = document.createElement("li")
+
+            li.innerText = comment
+            ul.append(li)  
+        })
+
+    
+       
+        
 }
 
 window.onload = initializePage
